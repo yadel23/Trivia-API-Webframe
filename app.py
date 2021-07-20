42bfbc9b6926e707747c4491e0e4e1abe1f96393
Ayeyi <asaturks24@gmail.com>
2021-07-20 16:36:27 -0400

    Fixed home and info page

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from flask import Flask, render_template, session, redirect, url_for, jsonify, r
 app = Flask(__name__)
 
 @app.route("/")
+@app.route("/home")
 def home():
     return render_template("home.html")
 
