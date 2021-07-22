5c1eb77f6b17b1b91e1bd49bb77450992b2fea45
Andrew Mauney <barca.aj@gmail.com>
2021-07-22 15:43:57 +0000

    style checker, unit tests, and etc

--- app.py
+++ app.py
@@ -1,16 +1,23 @@
-from flask import Flask, render_template, session, redirect, url_for, jsonify, request
-import requests, random, html
+import html
+import random
+import requests
+from flask import Flask, render_template,
+session, redirect, url_for, jsonify, request
 app = Flask(__name__)
 
 
 app.config['SECRET_KEY'] = '51'
 
+
 @app.route("/quiz")
 def quiz():
     return render_template("quiz.html")
 
+
 question_list = []
-@app.route("/user/input", methods = ["POST"])
+
+
+@app.route("/user/input", methods=["POST"])
 def user_input():
     global question_list
     global correct_answers
@@ -20,49 +27,39 @@ def user_input():
     category = request.form.get("category")
     difficulty = request.form.get("difficulty")
     typeQ = request.form.get("type")
-    
-    url = getUrl(amount, category , difficulty, typeQ)
+
+    url = getUrl(amount, category, difficulty, typeQ)
     Json = getJson(url)
     correct_answers, final_answers, question_list = toDict(Json)
-    
-    #session['correct_answers'] = correct_answers
-    #session["final_answers"] = final_answers
-    #session["question_list"] = question_list
-    #print('INITIAL LIST: ', question_list)
-    
 
-    #next_que = [0]  
-   # session["next_que"] = next_que
-    #quiz(correct_answers,final_answers,question_list) 
-    #next_question(correct_answers,final_answers,question_list)
+    return quiz(correct_answers, final_answers, question_list)
 
-    return quiz(correct_answers,final_answers,question_list)
-   
 
 def getUrl(amount, category, difficulty, typeQ):
     Base_url = 'https://opentdb.com/api.php?amount=' + str(amount)
     final_url = Base_url
-   #categoryA
+    # category
     if category != 'default_c':
-        final_url =final_url + '&category=' + str(category) 
-      
-   #difficulty
+        final_url = final_url + '&category=' + str(category)
+        Trivia - API - Webframe / README.md
+
+    # difficulty
     if difficulty != 'default_d':
-        final_url = final_url + '&difficulty=' + str(difficulty) 
-      
-    #type  
+        final_url = final_url + '&difficulty=' + str(difficulty)
+
+    # type
     if typeQ != 'default_t':
-        final_url = final_url + '&type=' + str(typeQ) 
-      
+        final_url = final_url + '&type=' + str(typeQ)
+
     return final_url
-   
-   
+
+
 def getJson(final_url):
     response = requests.get(final_url)
     data = response.json()
     return data
 
-      
+
 def toDict(json_data):
     question_list = {}
     correct_list = []
@@ -70,8 +67,8 @@ def toDict(json_data):
     answers = []
     final_answers = []
     for value in json_data['results']:
-        #print(value)
-        #questions.append(value['question'])
+        # print(value)
+        # questions.append(value['question'])
         question_list[value['question']] = value['type']
         correct_list.append(value['correct_answer'])
         correct = value['correct_answer']
@@ -79,10 +76,10 @@ def toDict(json_data):
         answers.append(correct)
         random.shuffle(answers)
         final_answers.append(answers)
-    print('q list ', question_list)
-   # print('f answers ', final_answers)
-    #print('c list' , correct_list)
-    
+    # print('q list ', question_list)
+    # print('f answers ', final_answers)
+    # print('c list' , correct_list)
+
     return correct_list, final_answers, question_list
 
 
@@ -96,78 +93,102 @@ def home():
 def quiz_2():
     return render_template("quiz.html")
 
+
 @app.route("/info")
 def info():
     return render_template("info.html")
 
 
-
-def quiz(correct_answers,final_answers,question_list):
-    #redirect("/quiz")
+def quiz(correct_answers, final_answers, question_list):
     question_type = list(question_list.values())[0]
-        #print(question_type)                     
-       # print(next(iter(question_list)))                   
-    question_name = list(question_list.keys())[0]                    
-    
-    if question_type == 'multiple':            
-        return render_template('quiz.html', question = '1) ' + html.unescape(question_name), answer1 = html.unescape(final_answers[0][0]), answer2 = html.unescape(final_answers[0][1]), answer3 = html.unescape(final_answers[0][2]), answer4 = html.unescape(final_answers[0][3]))
-              
-                            
+    question_name = list(question_list.keys())[0]
+
+    if question_type == 'multiple':
+        return render_template(
+            'quiz.html',
+            question='1) ' +
+            html.unescape(question_name),
+            answer1=html.unescape(
+                final_answers[0][0]),
+            answer2=html.unescape(
+                final_answers[0][1]),
+            answer3=html.unescape(
+                final_answers[0][2]),
+            answer4=html.unescape(
+                final_answers[0][3]))
+
     if question_type == 'boolean':
-        return render_template("quiz.html", question = '1) ' + html.unescape(question_name), answer1 = 'True', answer2 = 'False')
-                            
+        return render_template(
+            "quiz.html",
+            question='1) ' +
+            html.unescape(question_name),
+            answer1='True',
+            answer2='False')
+
 
 next_que = 0
 score = 0
-@app.route('/next/question', methods = ["POST"])
+
+
+@app.route('/next/question', methods=["POST"])
 def next_question():
     global next_que
     global question_list
     global correct_answers
-    global final_answers 
+    global final_answers
     global amount
     global score
 
     answer = request.form.get("answers")
-    
+
     print('outside', final_answers[next_que][int(answer)])
     print('outside', correct_answers[next_que])
-    if(final_answers[next_que][int(answer)] == correct_answers[next_que]): 
+    if(final_answers[next_que][int(answer)] == correct_answers[next_que]):
         print(final_answers[next_que][int(answer)])
         print(correct_answers[next_que])
-        score+=1
+        score += 1
 
     if int(next_que + 1) == int(amount):
         print('\n\n\nLAST QUESTION\n\n\n\n')
-        #return '<script>window.alert("You are done, score is "){score}</script>'
+        # return '<script>window.alert("You are done, score is
+        # "){score}</script>'
         print(score)
         return home()
     next_que += 1
     question_type = list(question_list.values())[next_que]
-    question_name = list(question_list.keys())[next_que]                    
-    #print(question_name) 
+    question_name = list(question_list.keys())[next_que]
+    # print(question_name)
 
-    
     print(next_que)
     print('AMT: ', amount)
-    
-    
-      
+
     if question_type == 'multiple':
         print(final_answers)
-        return render_template('quiz.html',  question = str(next_que + 1) + ") "+ html.unescape(question_name), answer1 = html.unescape(final_answers[next_que][0]), answer2 = html.unescape(final_answers[next_que][1]), answer3 = html.unescape(final_answers[next_que][2]), 
-                                   answer4 = html.unescape(final_answers[next_que][3]))
-              
-                            
-    if question_type == 'boolean':   
+        return render_template(
+            'quiz.html',
+            question=str(
+                next_que + 1) + ") " + html.unescape(question_name),
+            answer1=html.unescape(
+                final_answers[next_que][0]),
+            answer2=html.unescape(
+                final_answers[next_que][1]),
+            answer3=html.unescape(
+                final_answers[next_que][2]),
+            answer4=html.unescape(
+                final_answers[next_que][3]))
+
+    if question_type == 'boolean':
         print(final_answers)
-        return render_template("quiz.html", question = str(next_que + 1) + ") "+ html.unescape(question_name), answer1 = 'True', answer2 = 'False')
+        return render_template(
+            "quiz.html",
+            question=str(
+                next_que +
+                1) +
+            ") " +
+            html.unescape(question_name),
+            answer1='True',
+            answer2='False')
 
-      
-   
 
-      
 if __name__ == '__main__':
-    app.run(debug = True, host="0.0.0.0")
-    
-    
+    app.run(debug=True, host="0.0.0.0")
--- tester.py
+++ tester.py
@@ -1,8 +1,44 @@
-import unittest
 from app import user_input, getUrl, getJson, toDict
-from flask import Flask, render_template, session, redirect, url_for, jsonify, request
+import unittest, sys
+
+sys.path.append('../Trivia-API-Webframe') # imports python file from parent directory
+from app import app #imports flask app object
+
+class BasicTests(unittest.TestCase):
+
+    # executed prior to each test
+    def setUp(self):
+        self.app = app.test_client()
+
+    ###############
+    #### tests ####
+    ###############
+
+    def test_main_page(self):
+        response = self.app.get('/', follow_redirects=True)
+        self.assertEqual(response.status_code, 200)
+    
+    def home(self):
+        response = self.app.get('/home', follow_redirects=True)
+        self.assertEqual(response.status_code, 200)
+
+    def quiz(self):
+        response = self.app.get('/quiz', follow_redirects=True)
+        self.assertEqual(response.status_code, 200)
+
+    def info(self):
+        response = self.app.get('/info', follow_redirects=True)
+        self.assertEqual(response.status_code, 200)
+
+    def next_question(self):
+        response = self.app.get('/next/question', follow_redirects=True)
+        self.assertEqual(response.status_code, 200)
+        
 
 
-class TestFileName(unittest.TestCase):
+
+if __name__ == "__main__":
+    unittest.main()
+
 
     
